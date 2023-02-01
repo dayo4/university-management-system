7832d4155b9fdfbae27caef95cb5506be9848024
dayo4 <dayo4@live.com>
2023-02-01 06:37:54 +0100

    deleted old redundant codes

--- src/components/Management/Topnav/Topnav.js
+++ src/components/Management/Topnav/Topnav.js
@@ -103,42 +103,6 @@ const Topnav = (props) => {
         </div>
       </section>
 
-      {/* <div className="topnav-problem">
-        <div className="topnav-cont">
-          <p>{props.title}</p>
-
-          <form
-            // className={navshow ? "search-topnav-cont" : "nosearch-topnav-cont"}
-            className="search-topnav-cont"
-            autoCorrect="on"
-            autoComplete="on"
-          >
-            <button
-            //  onClick={() => setNavshow(!navshow)}
-             >
-              <img src="./search.svg" alt="" />
-            </button>
-            <input type="text" placeholder="Search..." required />
-
-            <div className="dis-dis" onClick={() => setNavshow(!navshow)}>
-              <img src="./search.svg" alt="" />
-            </div>
-          </form>
-
-          <div className="notification-user">
-            <img className="noti-icon" src="./notification.svg" alt="" />
-
-            <div className="topnav-user-cont">
-              <img src="./userimg.svg" alt="" />
-
-              <div className="user-names">
-                <h5>Admin</h5>
-                <h6>aggggede@gmail.com</h6>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </div>
   );
 };
