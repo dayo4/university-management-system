81f05e6f222cb1d5b9900c2cd65e549f424e8b4f
dayo4 <dayo4@live.com>
2023-02-02 10:13:10 +0100

    .

--- src/components/Student/Index.js
+++ src/components/Student/Index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { Routes, Route, NavLink } from "react-router-dom";
+import { Row, Col, } from 'antd';
 
 import NoAccess from "../InvalidAccess/noAccess";
 
@@ -7,7 +8,7 @@ import SideNav from "./Navs/SideNav/SideNav";
 import Topnav from "./Navs/TopNav/TopNav";
 
 /* Import students section routes */
-import Dashboard from "./Dashboard/Dashboard";
+import Dashboard from "./SubRoutes/Dashboard/Dashboard";
 
 const EntryPoint = ({ children }) => {
   const user = JSON.parse(localStorage.getItem("userData"));
@@ -21,14 +22,13 @@ const EntryPoint = ({ children }) => {
     <div id="MANAGEMENT-WHOLE">
       <Topnav title={"Dashboard"} />
       <SideNav onTabSwitch />
-      <div id="MARGIN-APP">
-        <div id="MARGIN-UP-FIXED">
+      <Row justify="center" id="MARGIN-APP" >
+        <Col span={22} id="MARGIN-UP-FIXED">
           <Routes>
             <Route path="/" element={<Dashboard />} />
           </Routes>
-          Students Home{children}
-        </div>
-      </div>
+        </Col>
+      </Row>
     </div>
   );
   // } else {
