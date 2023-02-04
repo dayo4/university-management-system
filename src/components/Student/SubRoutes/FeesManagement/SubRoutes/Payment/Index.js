87d57ea33dcf726972b91a8ddd5df705dd35edec
dayo4 <dayo4@live.com>
2023-02-04 07:54:06 +0100

    updated

---
+++ src/components/Student/SubRoutes/FeesManagement/SubRoutes/Payment/Index.js
@@ -0,0 +1,51 @@
+import React from 'react'
+import { Row, Col, Button } from 'antd';
+import Icon from '@mdi/react';
+import {
+  mdiCardsOutline,
+  mdiFileDocumentMultipleOutline,
+  mdiCommentTextOutline,
+  mdiDeleteOutline,
+  mdiAccountCircle
+} from '@mdi/js';
+import "./Index.scss"
+// Import dummy svg images
+import PaymentOpt from "../../images/PaymentOpt.svg"
+
+const Items = [
+  { title: 'Payment', link: 'payment', icon: mdiCardsOutline },
+  { title: 'Receipts', link: 'receipts', icon: mdiFileDocumentMultipleOutline },
+  { title: 'Payment Complaint', link: 'complaint', icon: mdiCommentTextOutline },
+]
+
+const Home = () => {
+  return (
+    <div>
+      <Row wrap justify="space-around" className="SubWrapper">
+        {Items.map((item, i) => {
+          return (
+            <Col key={i} className={"Box " + (i === 0 ? "Active" : "")} xs={6}>
+              <div className='Icon'>
+                <Icon path={item.icon}
+                  title={item.title}
+                  size={1}
+                />
+              </div>
+              <div className="Title">{item.title}</div>
+            </Col>
+          )
+        })}
+      </Row>
+      <Row justify="center" className="Content">
+        <Col xs={24} sm={20} md={20}>
+          <h5 className='Heading'>Payment Options</h5>
+        </Col>
+        <Col xs={24} sm={20} md={20}>
+          <img src={PaymentOpt}></img>
+        </Col>
+      </Row>
+    </div>
+  )
+}
+
+export default Home
